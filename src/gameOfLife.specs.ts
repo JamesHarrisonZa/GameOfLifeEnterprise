c6428e0a3c1e1d40025eb23c154c64c353fb6caa
James Harrison <JamesH@SAHL.COM>
2017-08-23 16:27:55 +0200

    Bug fix!

--- src/gameOfLife.specs.ts
+++ src/gameOfLife.specs.ts
@@ -5,7 +5,7 @@ describe('GameOfLife', () => {
 	const gameOfLife = new GameOfLife();
 	const startingCells = [[0,0],[0,0]];
 
-	describe('Should get next generation', () => {
+	it('Should get next generation', () => {
 
 		const expectedResult = [[0,0],[0,0]];
 		const nextGeneration = gameOfLife.getNextGeneration(startingCells);
