cd05d35225d4d7a6082384151d2ca91d75744f5a
James Harrison <JamesH@SAHL.COM>
2017-08-25 15:53:50 +0200

    Added

---
+++ src/startingCells.ts
@@ -0,0 +1,49 @@
+export class StartingCells {
+
+	private _fillPercentage = 40; //Disable animations if you want to fill more
+
+	public getCellsHeight(): number {
+
+		const viewPortHeigth = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		return this.getCellUnits(viewPortHeigth);
+	}
+
+	public getCellsWidth = () => {
+
+		const viewPortWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		return this.getCellUnits(viewPortWidth);
+	}
+
+	public getStartingCells(gridHeight: number, gridWidth: number): ReadonlyArray<ReadonlyArray<number>> {
+
+		const cells = new Array<number>(gridHeight)
+			.fill(null) //Cant apply map on undefined elements, need to set to null first
+			.map((_, y) => new Array<number>(gridWidth)
+				.fill(null)
+				.map((__, x) => {
+					return this.isInAreaToRandomise(gridHeight, gridWidth, y, x) ? this.randomBinary() : 0;
+				})
+			);
+
+		return cells;
+	}
+
+	private getCellUnits(viewPortSize: number):number {
+
+		const cellDimension = 10;
+		const extraMargin = 2;
+		return Math.floor(viewPortSize / cellDimension) - extraMargin;
+	}
+
+	private isInAreaToRandomise(gridHeight: number, gridWidth: number, y: number, x: number): boolean {
+
+		return y < gridHeight * this._fillPercentage / 100 && x < gridWidth * this._fillPercentage / 100;
+	}
+
+	private randomBinary(): number {
+
+		const max = 1;
+		const min = 0;
+		return Math.floor(Math.random() * (max - min + 1));
+	}
+}
