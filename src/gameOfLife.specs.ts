fa29bc496efba778dd967f8f85d3652f94f9f9af
James Harrison <JamesH@SAHL.COM>
2017-08-25 12:07:32 +0200

    More tests !

--- src/gameOfLife.specs.ts
+++ src/gameOfLife.specs.ts
@@ -4,85 +4,156 @@ describe('GameOfLife', () => {
 
 	const gameOfLife = new GameOfLife();
 
-	describe('Given a 2x2 grid with all dead cells', () => {
+	describe('Given a 2x2 grid', () => {
 
-		const startingCells =
-			[
-				[0, 0],
-				[0, 0]
-			];
+		describe('with all dead cells', () => {
 
-		it('The next generation should not change', () => {
-
-			const expectedResult =
+			const startingCells =
 				[
 					[0, 0],
 					[0, 0]
 				];
-			const nextGeneration = gameOfLife.getNextGeneration(startingCells);
-			expect(nextGeneration).toEqual(expectedResult);
+
+			it('the next generation should not change', () => {
+
+				const expectedResult =
+					[
+						[0, 0],
+						[0, 0]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
 		});
-	});
 
-	describe('Given a 2x2 grid with all live cells', () => {
+		describe('with all live cells', () => {
 
-		const startingCells =
-			[
-				[1, 1],
-				[1, 1]
-			];
-
-		it('The next generation should should not change', () => {
-
-			const expectedResult =
+			const startingCells =
 				[
 					[1, 1],
 					[1, 1]
 				];
-			const nextGeneration = gameOfLife.getNextGeneration(startingCells);
-			expect(nextGeneration).toEqual(expectedResult);
+
+			it('the next generation should should not change', () => {
+
+				const expectedResult =
+					[
+						[1, 1],
+						[1, 1]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
 		});
-	});
 
-	describe('Given a 2x2 grid with 3 live cells', () => {
+		describe('with a single live cell', () => {
 
-		const startingCells =
-			[
-				[1, 1],
-				[1, 0]
-			];
+			const startingCells =
+				[
+					[0, 0],
+					[0, 1]
+				];
 
-		it('The next generation should be all live cells', () => {
+			it('the next generation should kill the lonely cell :(', () => {
 
-			const expectedResult =
+				const expectedResult =
+					[
+						[0, 0],
+						[0, 0]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
+		});
+
+		describe('with a dead cell with 3 live neighbours', () => {
+
+			const startingCells =
 				[
 					[1, 1],
-					[1, 1]
+					[1, 0]
 				];
-			const nextGeneration = gameOfLife.getNextGeneration(startingCells);
-			expect(nextGeneration).toEqual(expectedResult);
+
+			it('the next generation should bring the dead cell to life', () => {
+
+				const expectedResult =
+					[
+						[1, 1],
+						[1, 1]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
 		});
 	});
 
-	describe('Given a 3x3 grid with all live cells', () => {
+	describe('Given a 3x3 grid', () => {
 
-		const startingCells =
-			[
-				[1, 1, 1],
-				[1, 1, 1],
-				[1, 1, 1]
-			];
+		describe('with all live cells', () => {
 
-		it('The next generation should be a cross of dead cells', () => {
-
-			const expectedResult =
+			const startingCells =
 				[
-					[1, 0, 1],
+					[1, 1, 1],
+					[1, 1, 1],
+					[1, 1, 1]
+				];
+
+			it('the next generation should have a cross of dead cells', () => {
+
+				const expectedResult =
+					[
+						[1, 0, 1],
+						[0, 0, 0],
+						[1, 0, 1]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
+		});
+
+		describe('with a single live cell', () => {
+
+			const startingCells =
+				[
 					[0, 0, 0],
-					[1, 0, 1]
+					[0, 1, 0],
+					[0, 0, 0]
 				];
-			const nextGeneration = gameOfLife.getNextGeneration(startingCells);
-			expect(nextGeneration).toEqual(expectedResult);
+
+			it('the next generation should kill the lonely cell :(', () => {
+
+				const expectedResult =
+					[
+						[0, 0, 0],
+						[0, 0, 0],
+						[0, 0, 0]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
+		});
+
+		describe('with 3 live cells', () => {
+
+			const startingCells =
+				[
+					[1, 1, 1],
+					[0, 0, 0],
+					[0, 0, 0]
+				];
+
+			it('the next generation should bring the dead cell to life', () => {
+
+				const expectedResult =
+					[
+						[0, 1, 0],
+						[0, 1, 0],
+						[0, 0, 0]
+					];
+				const nextGeneration = gameOfLife.getNextGeneration(startingCells);
+				expect(nextGeneration).toEqual(expectedResult);
+			});
 		});
 	});
+
 });
