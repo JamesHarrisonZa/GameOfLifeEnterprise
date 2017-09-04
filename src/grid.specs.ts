eccb6d76ab891da3d05c024823a607b935dcab10
James Harrison <JamesH@SAHL.COM>
2017-09-04 08:57:08 +0200

    WIP tests

--- src/grid.specs.ts
+++ src/grid.specs.ts
@@ -8,10 +8,10 @@ describe('Grid', () => {
 	const windowHeight = 1080;
 	const windowWidth = 1920;
 	const documentMock = TypeMoq.Mock.ofType<Document>();
-	const documentElementMock = TypeMoq.Mock.ofType<HTMLElement>();
-	documentElementMock.setup(p => p.clientHeight).returns(() => 1080);
-	documentElementMock.setup(p => p.clientWidth).returns(() => 1920);
-	documentMock.setup(p => p.documentElement).returns(() => documentElementMock.object);
+
+	//mocking document.querySelectorAll('.row');
+	//const row = new NodeListOf<Element>();
+	//documentMock.setup(p => p.querySelector).returns(() => );
 
 	const startingCells = new StartingCells(windowHeight, windowWidth);
 	const gameOfLife = new GameOfLife();
@@ -19,10 +19,15 @@ describe('Grid', () => {
 
 	describe('when updating the grid', () => {
 
+		// beforeEach(() => {
+		// 	spyOn(gameOfLife, 'getNextGeneration');
+		// 	grid.UpdateGrid();
+		// });
+
 		it('should be defined', () => expect(grid).toBeDefined());
 
 		// it('should get the next generation of the game of life', () => {
-
+		// 	expect(gameOfLife.getNextGeneration).toHaveBeenCalledTimes(1);
 		// });
 
 		// it('should check if each cell needs updating ', () => {
