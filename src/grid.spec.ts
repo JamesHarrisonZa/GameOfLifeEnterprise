9ba58e3032ee9ed1cb40a0d824e991e050c339be
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-07-18 12:23:56 +0200

    Redid tests

--- src/grid.spec.ts
+++ src/grid.spec.ts
@@ -4,32 +4,84 @@ import { GameOfLife } from './gameOfLife';
 
 describe('Grid', () => {
 
-	const windowHeight = 1080;
-	const windowWidth = 1920;
+	const _windowHeight = 1080;
+	const _windowWidth = 1920;
 	const _document = window.document;
 
-	const startingCells = new StartingCells(windowHeight, windowWidth);
-	const gameOfLife = new GameOfLife();
-	const grid = new Grid(startingCells, gameOfLife, _document);
+	describe('when created', () => {
 
-	// beforeEach(() => {
-	// 	var spiedFunction = spyOn(grid, 'createEmptyDivs').and.callThrough();
-	// });
+		let _startingCells: StartingCells;
+		let _gameOfLife: GameOfLife;
 
-	//it should create empty divs
+		beforeEach(() => {
+			_startingCells = new StartingCells(_windowHeight, _windowWidth);
+			_gameOfLife = new GameOfLife();
+		});
 
-	describe('after creating empty divs', () => {
+		it('should should the cell divs', () => {
+
+			const spy = spyOn<any>(_document, 'createElement').and.callThrough();
+			// tslint:disable-next-line:no-unused-expression
+			new Grid(_startingCells, _gameOfLife, _document);
+			expect(spy).toHaveBeenCalledTimes(190);
+		});
+
+		it('should append the grid div to the document', () => {
+
+			const spy = spyOn<any>(_document.body, 'appendChild').and.callThrough();
+			// tslint:disable-next-line:no-unused-expression
+			new Grid(_startingCells, _gameOfLife, _document);
+			expect(spy).toHaveBeenCalledTimes(1);
+		});
 
 		describe('when updating the grid', () => {
 
-			beforeEach(() => {
-				spyOn(gameOfLife, 'getNextGeneration').and.callThrough();
-				grid.update();
+			it('should get the next generation of the game of life', () => {
+				const sut = new Grid(_startingCells, _gameOfLife, _document);
+				const spy = spyOn(_gameOfLife, 'getNextGeneration').and.callThrough();
+				sut.update();
+				expect(spy).toHaveBeenCalledTimes(1);
 			});
 
-			it('should get the next generation of the game of life', () => expect(gameOfLife.getNextGeneration).toHaveBeenCalledTimes(1));
+			it('should check if each cell needs updating', () => {
+				const sut = new Grid(_startingCells, _gameOfLife, _document);
+				const spy = spyOn<any>(sut, 'cellNeedsUpdating').and.callThrough();
+				sut.update();
+				expect(spy).toHaveBeenCalledTimes(19928);
+			});
 
-			// it('should check if each cell needs updating ', () => );
+			it('should update the first batch of live cells', () => {
+				const sut = new Grid(_startingCells, _gameOfLife, _document);
+				const spy = spyOn<any>(sut, 'setIsActiveTrue').and.callThrough();
+				sut.update();
+				expect(spy).toHaveBeenCalled();
+			});
+
+			it('should not update any dead cells', () => {
+				const sut = new Grid(_startingCells, _gameOfLife, _document);
+				const spy = spyOn<any>(sut, 'setIsActiveFalse').and.callThrough();
+				sut.update();
+				expect(spy).not.toHaveBeenCalled();
+			});
+
+			describe('when updating the grid more than once', () => {
+
+				it('should update any new live cells', () => {
+					const sut = new Grid(_startingCells, _gameOfLife, _document);
+					const spy = spyOn<any>(sut, 'setIsActiveTrue').and.callThrough();
+					sut.update();
+					sut.update();
+					expect(spy).toHaveBeenCalled();
+				});
+
+				it('should any new dead cells', () => {
+					const sut = new Grid(_startingCells, _gameOfLife, _document);
+					const spy = spyOn<any>(sut, 'setIsActiveFalse').and.callThrough();
+					sut.update();
+					sut.update();
+					expect(spy).toHaveBeenCalled();
+				});
+			});
 		});
 	});
 });
